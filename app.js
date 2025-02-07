ae62ad6e460a90cdc5c9fd43db409373a3e51544
jcamilop <juank01102114@gmail.com>
2025-02-07 11:45:13 -0500

    task one

---
+++ app.js
@@ -0,0 +1,16 @@
+console.log('hola mundo');
+console.log(5 + 5);
+
+let nombre = 'camilo';
+let edad = 25;
+console.log(nombre);
+console.log(typeof(nombre));
+
+console.log('mi edad es '+ edad);
+console.log(typeof(edad));
+
+let tiene_trabajo = false;
+console.log(tiene_trabajo);
+console.log(typeof(tiene_trabajo));
+
+console.log(nombre + ' '  + edad + ' ' + tiene_trabajo);
